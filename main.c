92363386c402b3fbb49815027eff636ff00ed545
Nisoo1 <iguebg@gmail.com>
2026-02-23 15:32:48 -0500

    ...

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
-    LPSTR lpCmdLine, int nCmdShow)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+    PWSTR pCmdLine, int nCmdShow)
 {
-    MessageBoxA(NULL, "Goodbye, cruel world!", "Note", MB_OK);
+    MessageBoxW(NULL, L"Goodbye, cruel world!", "Note", MB_OK);
     return 0;
 }
